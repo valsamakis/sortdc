ecde1f930885f05d42beb02a0e6bd1caefa06104
Athanasios Valsamakis <athanasios.valsamakis@gmail.com>
2015-03-14 12:54:06 +0200

    Create sortdc.c

---
+++ sortdc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define frand() ((double) rand() / (RAND_MAX+1.0))
+
+typedef struct {
+    int data; // satellite data
+} node;
+
+int compare(const void *p1, const void *p2) {
+    const node *n1 = * (const node **) p1;
+    const node *n2 = * (const node **) p2;
+    
+    return n1->data - n2->data;
+}
+
+void sortdc(size_t size) {
+    node **array = (node **) malloc(size * sizeof (node *));
+    
+    int i = 0;
+    for (; i < size; i++) {
+        node *p = (node *) malloc(sizeof (node));
+        p->data = (int) (size * frand());
+        array[i] = p;
+    }
+    
+    for (i = 0; i < size; i++) {
+        printf("%d, ", array[i]->data );
+    }
+    
+    qsort(array, size, sizeof (node *), compare);
+
+    puts("\n");
+    for (i = 0; i < size; i++) {
+        printf("%d, ", array[i]->data );
+    }
+    puts("\n");
+}
