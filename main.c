1060330a80abf434facc940cf3cf11d1474dd0b9
Athanasios Valsamakis <athanasios.valsamakis@gmail.com>
2015-03-14 12:57:13 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,108 @@
+/* 
+ * File:   main.c
+ * Author: thanasis
+ *
+ * Created on March 1, 2015, 6:07 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+extern void sortdc(size_t);
+
+/*
+ * 
+ */
+int main(void) {
+
+    
+    
+//    struct fish snappy = {"Snappy", "Piranha", 69, 4};
+//    struct fish shark;
+//    
+//    
+//    printf("addr of snappy var %lu\n", &snappy);
+//    printf("addr of name var %lu\n", &snappy.name);
+//    printf("addr of species var %lu\n", &snappy.species);
+//    printf("addr of teeth var %lu\n", &snappy.teeth);
+//    printf("addr of age var %lu\n", &snappy.age);
+//    
+//    
+//    
+//    catalog(snappy);
+//    
+//    printf("age after call fun is %d\n", snappy.age);
+//    test();
+//    goo();
+    //make_tour();
+    //go_main();
+    //test2();
+    //filter();
+//    sortit();
+//    puts("------------");
+//    sortarea();
+//    puts("------------");
+//    puts("------------");
+    sortdc(100);
+    
+    return (EXIT_SUCCESS);
+}
+
+void catalog(struct fish f) {
+    
+    
+//    puts("-----");
+//    printf("addr of f var %lu\n", &f);
+//    printf("addr of name var %lu\n", &f.name);
+//    printf("addr of species var %lu\n", &f.species);
+//    printf("addr of teeth var %lu\n", &f.teeth);
+//    printf("addr of age var %lu\n", &f.age);
+//    f.age = 100;
+    
+}
+
+
+void test(int arg) {
+    int a;
+    struct fish shark;
+    struct fish fin;
+    char b;
+    
+//    puts("-- test ---");
+//    printf("addr of a var %lu\n", &a);
+//    printf("addr of arg var %lu %lu\n", &arg, sizeof(arg));
+//    printf("addr of b var %lu\n", &b);
+//    printf("addr of shark var %lu %lu\n", &shark, sizeof(shark));
+//    printf("addr of fin var %lu %lu\n", &fin, sizeof(fin));
+    
+}
+
+void goo() {
+    int a, b;
+    char (*p)[5];
+    quantity sold = {7};
+    sold.name = "aaaa";
+    sold.weight = 128;
+    enum colors {RED, GREEN, PUCE};
+    enum colors c;
+    c = RED;
+    
+//    printf("address of a %lu\n", &a);
+//    printf("address of b %lu\n", &b);
+//    printf("address of p %lu\n", &p);
+//    printf("address of (*p)[0] %lu\n", &((*p)[0]) );
+//    printf("address of (*p)[1] %lu\n", &((*p)[1]) );
+//    printf("address of (*p)[2] %lu\n", &((*p)[2]) );
+//    
+//    printf("size of short %lu\n", sizeof(short));
+//    printf("size of int %lu\n", sizeof(int));
+//    printf("size of char * %lu\n", sizeof(char *));
+//    printf("size of qantity %lu\n", sizeof(sold));
+    
+}
